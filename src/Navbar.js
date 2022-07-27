9a7331e4835bd1e7c8bac1baeac37693517ca96a
Yash Jangir <yashja23@gmail.com>
2022-07-27 20:45:37 +0530

    mobile navbar

--- src/Navbar.js
+++ src/Navbar.js
@@ -2,10 +2,28 @@ import React from 'react'
 import "./index.css"
 
 export default function Navbar(){
+    
+    const handleClick = () => {
+        const mNavbar = document.getElementById("mobileNavbar");
+        console.log(mNavbar.classList)
+        mNavbar.classList.toggle("d-none");
+        mNavbar.classList.toggle("mobile-navbar")
+    }
+
     return (
         <div className = "navbar-container">
            <img src = '/images/logo.svg' alt = "logo" className = 'logo' />
-           <i class="bars-icon fa-solid fa-bars"></i>
+           <i class="bars-icon fa-solid fa-bars d-none" id = "navbarBars" onClick = {handleClick}></i>
+
+           <div className = 'mobile-navbar' id = "mobileNavbar">
+              <div className = 'mobile-navbar-items-list'>
+                <p className = 'mobile-navbar-item'><a className = 'mobile-navbar-link' href = "google.com">About</a></p>
+                <p className = 'mobile-navbar-item'><a className = 'mobile-navbar-link' href = "google.com">Services</a></p>
+                <p className = 'mobile-navbar-item'><a className = 'mobile-navbar-link' href = "google.com">Projects</a></p>
+              </div>
+              <button className = 'mobile-contact-button'>Contact</button>
+           </div>
+
            <div className = 'navbar-contents-container'>
                 <ul className = 'navbar-items-container'>
                     <li className = 'navbar-item'><a className = 'navbar-links' href = "google.com">About</a></li>
